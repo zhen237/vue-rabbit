d1018dec649fae48e4b424b9de6727c7f86d626b
zhen237 <gao15135029905@163.com>
2025-05-11 19:44:43 +0800

    '吸顶导航交互事件'

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,11 +2,15 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
+
 </script>
 
 <template>
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
   <LayoutFooter />
+
 </template>
